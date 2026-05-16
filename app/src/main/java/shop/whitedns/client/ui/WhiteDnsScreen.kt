4780b58cde6c41aa7ef7087c20e11b2fa5797615
Danial Samadi <danisamadi11@gmail.com>
2026-05-16 16:43:26 -0400

    feat(a11y): import semantics modifier

--- app/src/main/java/shop/whitedns/client/ui/WhiteDnsScreen.kt
+++ app/src/main/java/shop/whitedns/client/ui/WhiteDnsScreen.kt
@@ -131,6 +131,8 @@ import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
